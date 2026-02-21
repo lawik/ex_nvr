b8bb541b106c8c3d09fc21a795519f5ecbd7298f
Lars Wikman <lars@underjord.io>
2026-02-21 14:46:57 +0100

    Set up nerves firmware for inference

--- nerves_community/mix.exs
+++ nerves_community/mix.exs
@@ -36,6 +36,8 @@ defmodule ExNVR.Nerves.MixProject do
       {:nerves_runtime, "~> 0.13.0"},
       {:nerves_pack, "~> 0.7.1", targets: @all_targets},
       {:ex_nvr, path: "../ui", env: env},
+      # {:hailo, github: "underjord/hailo", targets: @all_targets},
+      {:hailo, path: Path.expand("~/projects/hailo"), targets: @all_targets, override: true},
       {:circuits_gpio, "~> 2.1"},
       {:circuits_i2c, "~> 2.0"},
       {:ex_nvr_system_rpi4,
